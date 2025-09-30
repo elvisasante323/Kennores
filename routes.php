c10a4d15ea6a501b8487acaa585bbfd0a6ab3063
Elvis Asante <elvisasante5@yahoo.com>
2025-09-30 21:47:23 +0100

    Update routes.php
    Two files needed for app

--- routes.php
+++ routes.php
@@ -1,11 +1,4 @@
 <?php
 
 $router->get('/', 'index.php');
-$router->get('/about', 'about.php');
-$router->get('/join', 'join.php');
-
-$processController = "/requests/processForms.php";
-
-$router->post('/', $processController);
-$router->post('/about', $processController);
-$router->post('/join', $processController);
+$router->post('/', "/requests/processForms.php");
